eec8959912e7e545f7eb7f205a401ec923d2e851
Karan Chopra <karan.chopra1@dcmail.ca>
2026-04-03 17:04:24 -0400

    adding custom post

--- wp-content/themes/project/functions.php
+++ wp-content/themes/project/functions.php
@@ -9,3 +9,31 @@ function mytheme_enqueue_styles() {
 }
 
 add_action('wp_enqueue_scripts', 'mytheme_enqueue_styles');
+
+// adding custom post 
+function thread_theory_drops_cpt() {
+
+    $labels = array(
+        'name' => 'Drops',
+        'singular_name' => 'Drop',
+        'menu_name' => 'Drops',
+        'add_new' => 'Add New Drop',
+        'add_new_item' => 'Add New Drop',
+        'edit_item' => 'Edit Drop',
+        'new_item' => 'New Drop',
+        'view_item' => 'View Drop',
+        'all_items' => 'All Drops',
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => true,
+        'rewrite' => array('slug' => 'drops'),
+        'supports' => array('title','editor','thumbnail'),
+        'show_in_rest' => true
+    );
+
+    register_post_type('drops', $args);
+}
+add_action('init', 'thread_theory_drops_cpt');
